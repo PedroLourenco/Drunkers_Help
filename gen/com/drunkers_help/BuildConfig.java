d915121496a1861826953f755b9fc1dacf5f2ee4
PedroLourenco <PedroLourenco@sh_bcosta.Shortcut.local>
2013-07-17 17:21:39 +0100

    teste

---
+++ gen/com/drunkers_help/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.drunkers_help;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/drunkers_help/MainActivity.java
@@ -0,0 +1,23 @@
+package com.drunkers_help;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//teste
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
