6bcf2f0446345ef3271b23f9b0ee52b34b8a1f0f
mejaime <mejaime@mejaime-PC.lan>
2013-07-19 19:57:24 +0100

    main activity layout
    Signed-off-by: mejaime <mejaime@mejaime-PC.lan>

--- gen/com/drunkers_help/R.java
+++ gen/com/drunkers_help/R.java
@@ -86,9 +86,12 @@ public final class R {
         public static final int white_star=0x7f02003e;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int btnGetMoreResults=0x7f080001;
+        public static final int btnCounter=0x7f080001;
+        public static final int btnResetCounter=0x7f080002;
         public static final int gridview=0x7f080000;
+        public static final int menuExit=0x7f080006;
+        public static final int menuExport=0x7f080005;
+        public static final int menuHistory=0x7f080004;
         public static final int menu_search=0x7f080003;
     }
     public static final class layout {
@@ -100,7 +103,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bt_counter=0x7f050003;
+        public static final int bt_resetCounter=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int menu_exit=0x7f050005;
+        public static final int menu_export=0x7f050007;
+        public static final int menu_history=0x7f050006;
     }
     public static final class style {
         /** 
