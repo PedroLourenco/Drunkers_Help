fdf30d2c1469108d70a80c0c528853925a497a6a
PedroLourenco <pdrolourenco@gmail.com>
2013-09-20 14:36:47 +0100

    v1.0.0
    Signed-off-by: PedroLourenco <pdrolourenco@gmail.com>

--- src/com/drunkers_helper/util/globalconstant.java
+++ src/com/drunkers_helper/util/globalconstant.java
@@ -5,7 +5,7 @@ import com.drunkers_help.R;
 public final class globalconstant {
 	
 	//log variables
-	public static final boolean LOG = true;
+	public static final boolean LOG = false;
     public static String TAG = "Drunker's Helper";
 
 	public static double lat, lon;
