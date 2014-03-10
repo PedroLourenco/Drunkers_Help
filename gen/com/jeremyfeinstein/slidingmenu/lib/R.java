f9254d3e24f2ebcf5695cdad95f9efbbab4dbd3e
PedroLourenco <pdrolourenco@gmail.com>
2014-03-10 20:33:42 +0000

    Release 1.2.1
    Signed-off-by: PedroLourenco <pdrolourenco@gmail.com>

--- gen/com/jeremyfeinstein/slidingmenu/lib/R.java
+++ gen/com/jeremyfeinstein/slidingmenu/lib/R.java
@@ -29,7 +29,7 @@ public final class R {
 		public static final int margin = 0x7f040012;
 		public static final int right = 0x7f040011;
 		public static final int selected_view = 0x7f040014;
-		public static final int slidingmenumain = 0x7f04005b;
+		public static final int slidingmenumain = 0x7f040054;
 	}
 	public static final class layout {
 		public static final int slidingmenumain = 0x7f030027;
